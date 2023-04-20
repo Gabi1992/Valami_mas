d210263cbce9a394708f1393ec1ff3b8f3bd1192
Gabi1992 <nagy.gabi0202@gmail.com>
2023-04-20 13:38:47 +0200

    cr

--- backend/Carappointment/forms.py
+++ backend/Carappointment/forms.py
@@ -7,3 +7,15 @@ class MegrendeloForm(ModelForm):
         model = Megrendelo
         fields = ('nev','telefonszam','email','szolgaltatas','auto')
         
+        labels ={
+            'nev': '',
+            'telefonszam': '',
+            'email': '',
+        }
+        
+        widget = {
+            'nev': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Adja meg a nevét!'}),
+            'telefonszam': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Adja meg a telefonszámát!'}),
+            'email': forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Adja meg az email cimét!'}),
+        }
+        
