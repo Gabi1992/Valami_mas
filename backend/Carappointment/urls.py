6da60619611667a7a7c8fb27a5c176603f937b1d
Gabi1992 <nagy.gabi0202@gmail.com>
2023-04-20 17:09:08 +0200

    fr

--- backend/Carappointment/urls.py
+++ backend/Carappointment/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('szolgaltatas/<int:pk>',views.SzolgaltatasByID),
     path('createszolg/',views.CreateSzolgaltatas),
     path('updateszolg/<int:pk>',views.UpdateSzolgaltatas),
+    path('deleteszolg/<int:pk>',views.DeleteSzolgaltatas),
     path('autok/',views.MindenAuto),
     path('auto/<int:pk>',views.AutoByID),
     path('createauto/',views.CreateAuto),
