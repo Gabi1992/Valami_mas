beacc31cc4f90dd98226daaa1a3852d3a7f805e9
Gabi1992 <nagy.gabi0202@gmail.com>
2023-04-20 17:18:45 +0200

    ft

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("Carappointment.urls")),
+    path('api/', include("Carappointment.urls")),
 ]
